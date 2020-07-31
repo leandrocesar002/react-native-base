c9a898bd7a59979edd287e45d4f5596c4cd8ca23
Leandro Santos <leandrocmsantos@gmail.com>
2020-07-31 10:45:04 -0300

    config reactotron

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+import './config/ReactotronConfig';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5fcff',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
+
+console.tron.warn('Hello World');
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Welcome to react</Text>
+      <Text style={styles.welcome}>Abaixo</Text>
+    </View>
+  )
+}
+
+
