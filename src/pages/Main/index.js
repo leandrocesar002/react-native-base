cafe93cbbd4469b26bc2651d6e5a94aae2cf04a1
Leandro Santos <leandrocmsantos@gmail.com>
2020-08-04 00:47:06 -0300

    Adiciona chamado com api github

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,62 @@
-import React from 'react';
-import { Container } from './styles';
+/* eslint-disable react/state-in-constructor */
+/* eslint-disable lines-between-class-members */
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
+import { Keyboard } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import api from '../../services/api';
 
-export default function Main() {
-  return <Container />;
+import { Container, Form, SubmitButton, Input } from './styles';
+
+
+export default class Main extends Component {
+  state = {
+    newUser: '',
+    users: [],
+  };
+
+  handleAddUser = async () => {
+    const { users, newUser } = this.state;
+
+    const response = await api.get(`/users/${newUser}`);
+
+    const data = {
+      name: response.data.name,
+      login: response.data.login,
+      bio: response.data.bio,
+      avatar: response.data.avatar_url,
+    };
+
+    this.setState({
+      users: [...users, data],
+      newUser: '',
+    });
+
+    Keyboard.dismiss();
+  };
+
+  render() {
+    const { users, newUser } = this.state;
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Adicionar usuário"
+            value={newUser}
+            onChangeText={text => this.setState({ newUser: text })}
+            returnKeyType="send"
+            onSubmitEditing={this.handleAddUser}
+          />
+          <SubmitButton onPress={this.handleAddUser}>
+            <Icon name="add" size={20} color="#FFF" />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
-
 Main.navigationOptions = {
   title: 'Usuários',
 }
