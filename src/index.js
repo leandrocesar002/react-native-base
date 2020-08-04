61bf94ab370cfc2aae76f8b086a729b4afc71b9d
Leandro Santos <leandrocmsantos@gmail.com>
2020-08-03 23:11:51 -0300

    add styled components

--- src/index.js
+++ src/index.js
@@ -1,31 +1,16 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StatusBar } from 'react-native';
 
 import './config/ReactotronConfig';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f5fcff',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
-
-console.tron.warn('Hello World');
+import Routes from './routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to react</Text>
-      <Text style={styles.welcome}>Abaixo</Text>
-    </View>
-  )
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <Routes />
+    </>
+  );
 }
 
 
